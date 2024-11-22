11a862800f67a52c3512d345ab8ee5d44e0724f9
Elvis-199-afk <earocutipaq@unjbg.edu.pe>
2024-11-22 15:24:50 -0500

    Agregando el .cpp que permite escribir en un archivo .txt mediante el programa

---
+++ escribir_archivos.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+using namespace std;
+#include<fstream>
+
+int main(){
+	string ruc,producto;
+	int unidades;
+	double precio;
+	char aux;
+	ofstream archivo;
+	archivo.open("output.txt");
+	if(archivo.is_open()){
+		cout<<"RUC del cliente (X para terminar): ";
+		cin>>ruc;
+		while(ruc!="X"){
+			cin.get(aux);
+			cout<<"Producto: ";
+			getline(cin,producto);
+			cout<<"Unidades: ";
+			cin>>unidades;
+			cout<<"Precio: ";
+			cin>>precio;
+			
+			archivo<<ruc<<" "<<unidades<<" "<<precio<<" "<<producto<<endl;
+			cout<<"RUC del cliente (X para terminar): ";
+			cin>>ruc;
+		}
+	}
+	archivo<<"X";
+	archivo.close();
+	return 0;
+}
